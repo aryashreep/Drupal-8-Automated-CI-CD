376c5d568fbac53f97de89eac1821f0e977bca3e
Aryashree Pritikrishna <M1034940@mindtree.com>
2020-04-13 19:17:29 +0530

    Update the CICD config

--- .github/config/settings.local.php
+++ .github/config/settings.local.php
@@ -9,10 +9,11 @@ $databases['default']['default'] = [
   'username' => 'root',
   'password' => 'root',
   'prefix' => '',
-  'host' => '',
+  'host' => '127.0.0.1',
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
+  'unix_socket' => '/var/run/mysqld/mysqld.sock',
 ];
 
 // Display errors.
